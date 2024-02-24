cffabe89574c6324ee9072efb93687cca34039e9
lbvargas <luisbyronvargaspenafiel@gmail.com>
2024-02-23 22:54:42 -0500

    creation of projects and dependencies to be used

---
+++ ms-comp-banking-query/src/test/java/com/devluis/banking/account/query/QueryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devluis.banking.account.query;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
